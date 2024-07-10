3f3b2fe5059df6adeb87375065a5cfc4b7f6c803
Ghulam e mustafa <109214910+Gm1654@users.noreply.github.com>
2024-07-10 06:48:21 -0700

    Update main.py

--- Day 45 movie scraper/main.py
+++ Day 45 movie scraper/main.py
@@ -3,7 +3,6 @@ from bs4 import BeautifulSoup
 
 URL = "https://www.empireonline.com/movies/features/best-movies-2/"
 
-# Write your code below this line 👇
 
 response = requests.get(URL)
 website_html = response.text
